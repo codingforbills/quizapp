b22c218e2cf6f4a5439eed8e3f13e50f90ae8919
Robert Bills <78573697+rbills17@users.noreply.github.com>
2021-04-24 20:52:58 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -119,6 +119,126 @@ const questions = [
         answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
         correct: 4
     },
+    //1
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //2
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //3
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //4
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //5
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //6
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //7
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //8
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    }, //9
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //10
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //11
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //12
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //13
+
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //14
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //15
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //16
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //17
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //18
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //19
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
+    //40
+    {
+        question: "What does HTML stand for???",
+        answer: ['Hyper-Text Marketing Linguistic', 'Hiding-Tiger moving Lazer', 'Help-Tommy Make Lasagna', 'Hyper-Text Marketing Language'],
+        correct: 4
+    },
 ]
 var questionIndex = 0;
 var scorePoints = 0;
@@ -128,9 +248,12 @@ var TimerElement = $(".quiz_timer .time")
 
 $(document).ready(function () {
     loadQuiz();
-    refreshQuiz();
 });
+
 function loadQuiz() {
+    questionIndex = 0;
+    scorePoint = 0;
+    timer = 30;
     $(".actions_btns .btn-start").html("Start Quiz").fadeIn(1000)
         .html("Start")
         .fadeIn(500)
@@ -140,67 +263,82 @@ function loadQuiz() {
             setTimeout(() => {
                 $(".quiz_stats").fadeIn(500)
                 $(".question_wrapper").fadeIn(500)
-                newFunction();
+                loadNewQuestion();
             }, 1000);
         });
-
-    function newFunction() { //loadNewQuestion
-        if (questionIndex > questions.length) {
-
-            $(".quiz_wrapper".fadeOut();
-        }
-        else{
-            loadQuestion();
-            refreshQuiz();
-            $(".options_wrapper .option").hide();
-            $(".options_wrapper .option").removeClass("green");
-            $(".options_wrapper .option").removeClass("red");
-            $(".action_btns .btn-start").hide();
-            $(".options_wrapper").fadeIn(1000);
-            setTimeout(() => {
-                $(".options_wrapper .option:nth-child(1)").slideDown(500);
-                setTimeout(() => {
-                    $(".options_wrapper .option:nth-child(2)").slideDown(500);
-                    $(".actions_btns .btn-start").off("click");
-                    $(".options_wrapper.option").on("click", (e) => {
-                        checkAnswer(e);
-                    })
-                }, 500);
-            }, 1000);
-        } 
-    }
-
-    function checkAnswer(e) {
-        EventClass = e.target.classList;
-        ansStatus = 0;
-        if ((EventClass.contains("one") && questions[questionIndex].correct == 1) ||
-            (EventClass.contains("two") && questions[questionIndex].correct == 2) ||
-            (EventClass.contains("three") && questions[questionIndex].correct == 3) ||
-            (EventClass.contains("four") && questions[questionIndex].correct == 4)) {
-            EventClass.add("green");
-            ansStatus = 1;
-        }
-        else {
-
-            EventClass.add("red");
-        }
-        questionIndex++;
-        if (ansStatus == 1) {
-            scorePoint = scorePoint + 5;
-        }
-        $(".options_wrapper .option").off("click");
-        if (questionIndex > questions.length) {
-            $(".actions_btns .btn-start").html("Results").slideDown("2000").on("click", () =>
-            newFunction());
-    }
-        else {
-        $(".actions_btns .btn-start").html("Next Question").slideDown("2000").on("click", () =>
-            newFunction());
-
+    refreshQuiz();
 }
-refreshQuiz();
 
-loadQuestion();
+function loadNewQuestion() { //loadNewQuestion
+    if (questionIndex >= questions.length) {
+        $(".rScore").html(scorePoint);
+        $(".rQuestions").html(questions.length);
+        $(".rCorrect").html(scorePoint / 5);
+        $(".rWrong").html(questions.length - (ScorePoint / 5));
+        $(".rPercentage").html(`{((scorePoint / 5) - questions.length * 100).toFixed(1)}%`);
+        $(".quiz_wrapper".hide());
+        $(".results_wrapper").fadeIn();
+    } else {
+        loadQuestion();
+        refreshQuiz();
+        TimerElement.html(timer);
+        $(".options_wrapper .option").hide();
+        $(".options_wrapper .option").removeClass("green");
+        $(".options_wrapper .option").removeClass("red");
+        $(".action_btns .btn-start").hide();
+        $(".options_wrapper").fadeIn(1000);
+        setTimeout(() => {
+            $(".options_wrapper .option:nth-child(1)").fadeIn(500);
+            setTimeout(() => {
+                $(".options_wrapper .option:nth-child(2)").fadeIn(500);
+                $(".actions_btns .btn-start").off("click");
+                $(".options_wrapper .option").on("click", (e) => checkAnswer(e));
+                Counter = setInterval(TimeInterval, 1000)
+            }, 500);
+        }, 1000);
+    }
+}
+
+function TimeInterval() {
+    timer--;
+    TimerElement.html(timer);
+    if (timer <= 0) {
+        TimerElement.html("0");
+        $(`.options_wrapper .option:nth-child(${questions[questionIndex].correct})`).click();
+        scorePoint = scorePoint - 5;
+        clearInterval(Counter);
+        timer = 30;
+    }
+}
+
+function checkAnswer(e) {
+    EventClass = e.target.classList;
+    ansStatus = 0;
+    if ((EventClass.contains("one") && questions[questionIndex].correct == 1) ||
+        (EventClass.contains("two") && questions[questionIndex].correct == 2) ||
+        (EventClass.contains("three") && questions[questionIndex].correct == 3) ||
+        (EventClass.contains("four") && questions[questionIndex].correct == 4)) {
+        EventClass.add("green");
+        ansStatus = 1;
+    } else {
+
+        EventClass.add("red");
+        $(".points").html(scorePoint);
+    }
+    questionIndex++;
+    $(".options_wrapper .option").off("click");
+    clearInterval(Counter);
+    timer = 30;
+    if (questionIndex >= questions.length) {
+        $(".actions_btns .btn-start").html("Results").slideDown("2000").on("click", () => {
+            loadNewQuestion();
+        });
+    } else {
+        $(".actions_btns .btn-start").html("Next Question").slideDown("2000").on("click", () => {
+            loadNewQuestion();
+        });
+    }
+}
 
 function loadQuestion() {
     $(".question_wrapper .question .txt").html(questions[questionIndex].question);
@@ -210,13 +348,10 @@ function loadQuestion() {
 
     }
 }
+
 function refreshQuiz() {
 
     $(".total").html(questions.length);
     $(".answered").html(questionIndex + 1);
     $(".points").html(scorePoint);
 }
-    }
-}
-
-
